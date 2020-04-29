d5888f6d99109729f61ac164a27f24044d88e586
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2020-04-29 17:05:42 +0200

    noise std files

---
+++ hidimstat/test/test_noise_std.py
@@ -0,0 +1,28 @@
+"""
+Test the noise_std module
+"""
+
+import numpy as np
+from nose.tools import assert_almost_equal
+
+from noise_std import reid
+
+
+def test_reid():
+
+    np.random.seed(0)
+
+    n_samples, n_features = 30, 30
+    n_support = 10
+    sigma = 2.0
+
+    X = np.random.randn(n_samples, n_features)
+    beta = np.zeros(n_features)
+    beta[:n_support] = 1.0
+    epsilon = sigma * np.random.randn(n_samples)
+    y = np.dot(X, beta) + epsilon
+
+    sigma_hat = reid(X, y)
+    expected = sigma
+
+    assert_almost_equal(sigma_hat / expected, 1.0, places=0)
