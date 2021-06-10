65d30c57fb0ffb904bf60de306691c47a9be72a0
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2021-06-10 19:01:26 +0200

    testing new simu

--- hidimstat/test/test_scenario.py
+++ hidimstat/test/test_scenario.py
@@ -7,6 +7,7 @@ from numpy.testing import assert_almost_equal, assert_equal
 
 from hidimstat.scenario import multivariate_1D_simulation
 from hidimstat.scenario import multivariate_simulation
+from hidimstat.scenario import multivariate_temporal_simulation
 
 ROI_SIZE_2D = 2
 SHAPE_2D = (12, 12)
@@ -82,3 +83,30 @@ def test_multivariate_simulation():
 
     assert_equal(X.shape, (n_samples, shape[0] * shape[1] * shape[2]))
     assert_equal(np.count_nonzero(beta), 5 * (roi_size ** 3))
+
+
+def test_multivariate_temporal_simulation():
+
+    n_samples = 30
+    n_features = 50
+    n_targets = 10
+    support_size = 2
+    sigma = 1.0
+    rho = 0.9
+
+    X, Y, Beta, E = \
+        multivariate_temporal_simulation(n_samples=n_samples,
+                                         n_features=n_features,
+                                         n_targets=n_targets,
+                                         support_size=support_size,
+                                         sigma=sigma, rho=rho)
+
+    sigma_hat = np.std(E[:, -1])
+    rho_hat = np.corrcoef(E[:, -1], E[:, -2])[0, 1]
+
+    assert_almost_equal(sigma_hat, sigma, decimal=1)
+    assert_almost_equal(rho_hat, rho, decimal=1)
+    assert_equal(X.shape, (n_samples, n_features))
+    assert_equal(Y.shape, (n_samples, n_targets))
+    assert_equal(np.count_nonzero(Beta), support_size * n_targets)
+    assert_equal(Y, np.dot(X, Beta) + E)
