b9c317ce9c5f348df88c162eefdc61666b5a5b1f
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2021-06-11 15:54:40 +0200

    changing variable names as requested

--- hidimstat/test/test_scenario.py
+++ hidimstat/test/test_scenario.py
@@ -24,21 +24,21 @@ def test_multivariate_1D_simulation():
     rho = 0.7
     sigma = 1.0
 
-    X, y, beta, epsilon = \
+    X, y, beta, noise = \
         multivariate_1D_simulation(n_samples=n_samples, n_features=n_features,
                                    support_size=support_size, sigma=sigma,
                                    rho=rho, shuffle=False, seed=0)
 
-    sigma_hat = np.std(epsilon)
+    sigma_hat = np.std(noise)
     rho_hat = np.corrcoef(X[:, 19], X[:, 20])[0, 1]
 
     assert_almost_equal(sigma_hat, sigma, decimal=1)
     assert_almost_equal(rho_hat, rho, decimal=1)
     assert_equal(X.shape, (n_samples, n_features))
     assert_equal(np.count_nonzero(beta), support_size)
-    assert_equal(y, np.dot(X, beta) + epsilon)
+    assert_equal(y, np.dot(X, beta) + noise)
 
-    X, y, beta, epsilon = \
+    X, y, beta, noise = \
         multivariate_1D_simulation()
     rho_hat = np.corrcoef(X[:, 19], X[:, 20])[0, 1]
     assert_almost_equal(rho_hat, 0, decimal=1)
@@ -54,14 +54,14 @@ def test_multivariate_simulation():
     rho_expected = 0.8
     return_shaped_data = True
 
-    X, y, beta, epsilon, X_, w = \
+    X, y, beta, noise, X_, w = \
         multivariate_simulation(n_samples=n_samples, shape=shape,
                                 roi_size=roi_size, sigma=sigma,
                                 smooth_X=smooth_X,
                                 return_shaped_data=return_shaped_data,
                                 seed=0)
 
-    sigma_hat = np.std(epsilon)
+    sigma_hat = np.std(noise)
     rho_hat = np.corrcoef(X[:, 19], X[:, 20])[0, 1]
 
     assert_almost_equal(sigma_hat, sigma, decimal=1)
@@ -69,13 +69,13 @@ def test_multivariate_simulation():
     assert_equal(X.shape, (n_samples, shape[0] * shape[1]))
     assert_equal(X_.shape, (n_samples, shape[0], shape[1]))
     assert_equal(np.count_nonzero(beta), 4 * (roi_size ** 2))
-    assert_equal(y, np.dot(X, beta) + epsilon)
+    assert_equal(y, np.dot(X, beta) + noise)
 
     shape = SHAPE_3D
     roi_size = ROI_SIZE_3D
     return_shaped_data = False
 
-    X, y, beta, epsilon = \
+    X, y, beta, noise = \
         multivariate_simulation(n_samples=n_samples, shape=shape,
                                 roi_size=roi_size,
                                 return_shaped_data=return_shaped_data,
@@ -89,24 +89,24 @@ def test_multivariate_temporal_simulation():
 
     n_samples = 30
     n_features = 50
-    n_targets = 10
+    n_times = 10
     support_size = 2
     sigma = 1.0
     rho = 0.9
 
-    X, Y, Beta, E = \
+    X, Y, beta, noise = \
         multivariate_temporal_simulation(n_samples=n_samples,
                                          n_features=n_features,
-                                         n_targets=n_targets,
+                                         n_times=n_times,
                                          support_size=support_size,
                                          sigma=sigma, rho=rho)
 
-    sigma_hat = np.std(E[:, -1])
-    rho_hat = np.corrcoef(E[:, -1], E[:, -2])[0, 1]
+    sigma_hat = np.std(noise[:, -1])
+    rho_hat = np.corrcoef(noise[:, -1], noise[:, -2])[0, 1]
 
     assert_almost_equal(sigma_hat, sigma, decimal=1)
     assert_almost_equal(rho_hat, rho, decimal=1)
     assert_equal(X.shape, (n_samples, n_features))
-    assert_equal(Y.shape, (n_samples, n_targets))
-    assert_equal(np.count_nonzero(Beta), support_size * n_targets)
-    assert_equal(Y, np.dot(X, Beta) + E)
+    assert_equal(Y.shape, (n_samples, n_times))
+    assert_equal(np.count_nonzero(beta), support_size * n_times)
+    assert_equal(Y, np.dot(X, beta) + noise)
