aa0bdca5bc4f4911aad56b6cf49da969a837a33c
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2021-06-14 16:56:00 +0200

    Cleaning/updating Desparsified Lasso code (and tests)

--- hidimstat/desparsified_lasso.py
+++ hidimstat/desparsified_lasso.py
@@ -1,14 +1,44 @@
 import numpy as np
 import scipy.stats as st
 from joblib import Parallel, delayed
-from sklearn.linear_model import Lasso, LassoCV
+from sklearn.utils.validation import check_memory
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Lasso
 
 from .noise_std import reid
 
 
+def _compute_all_residuals(X, alphas, Gram=None, max_iter=5000, tol=1e-3,
+                           method='lasso', c=0.01, n_jobs=1, verbose=0):
+    """Nodewise Lasso. Compute all the residuals: regressing each column of the
+    design matrix against the other columns"""
+
+    n_samples, n_features = X.shape
+
+    results = \
+        Parallel(n_jobs=n_jobs, verbose=verbose)(
+            delayed(_compute_residuals)
+                (X=X,
+                 column_index=i,
+                 alpha=alphas[i],
+                 Gram=Gram,
+                 max_iter=max_iter,
+                 tol=tol,
+                 method=method,
+                 c=c)
+            for i in range(n_features))
+
+    results = np.asarray(results)
+    Z = np.stack(results[:, 0], axis=1)
+    omega_diag = np.stack(results[:, 1])
+
+    return Z, omega_diag
+
+
 def _compute_residuals(X, column_index, alpha=None, Gram=None, max_iter=5000,
                        tol=1e-3, method='lasso', c=0.01):
-    """Nodewise Lasso"""
+    """Compute the residuals of the regression of a given column of the
+    design matrix against the other columns"""
 
     n_samples, n_features = X.shape
     i = column_index
@@ -16,64 +46,85 @@ def _compute_residuals(X, column_index, alpha=None, Gram=None, max_iter=5000,
     X_new = np.delete(X, i, axis=1)
     y = np.copy(X[:, i])
 
-    if method == 'lasso' and Gram is None:
+    if method != 'lasso':
+        ValueError("The only regression method available is 'lasso'")
+
+    if Gram is None:
         Gram_loc = np.dot(X_new.T, X_new)
-    elif method == 'lasso' and Gram is not None:
+    else:
         Gram_loc = np.delete(np.delete(Gram, i, axis=0), i, axis=1)
 
-    if method == 'lasso' and alpha is None:
+    if alpha is None:
         k = c * (1. / n_samples)
         alpha = k * np.max(np.abs(np.dot(X_new, y)))
 
-    if method == 'lasso':
-        clf_lasso_loc = Lasso(alpha=alpha, precompute=Gram_loc,
-                              max_iter=max_iter, tol=tol)
+    clf_lasso_loc = Lasso(alpha=alpha, precompute=Gram_loc, max_iter=max_iter,
+                          tol=tol)
 
-    elif method == 'lasso_cv':
-        clf_lasso_loc = LassoCV(max_iter=max_iter, tol=tol, cv=3)
+    clf_lasso_loc.fit(X_new, y)
+    z = y - clf_lasso_loc.predict(X_new)
 
-    if method in ['lasso', 'lasso_cv']:
-        clf_lasso_loc.fit(X_new, y)
-        z = y - clf_lasso_loc.predict(X_new)
+    omega_diag_i = n_samples * np.sum(z ** 2) / np.dot(y, z) ** 2
 
-    omega_diag = n_samples * np.sum(z ** 2) / np.dot(y, z) ** 2
-
-    return z, omega_diag
+    return z, omega_diag_i
 
 
-def desparsified_lasso_confint(X, y, confidence=0.95, max_iter=5000,
-                               tol=1e-3, method="lasso", c=0.01, n_jobs=1):
+def desparsified_lasso_confint(X, y, normalize=True, dof_ajdustement=False,
+                               confidence=0.95, max_iter=5000, tol=1e-3,
+                               residual_method='lasso', c=0.01, n_jobs=1,
+                               memory=None, verbose=0):
+
     """Desparsified Lasso with confidence intervals
 
     Parameters
     -----------
-    X : ndarray or scipy.sparse matrix, (n_samples, n_features)
+    X : ndarray, shape (n_samples, n_features)
         Data.
 
-    y : ndarray, shape (n_samples,) or (n_samples, n_targets)
-        Target. Will be cast to X's dtype if necessary.
+    y : ndarray, shape (n_samples,)
+        Target.
 
-    confidence : float, optional
+    normalize : bool, optional (default=True)
+        If True, the regressors X will be normalized before regression and
+        the target y will be centered.
+
+    dof_ajdustement : bool, optional (default=False)
+        If True, makes the degrees of freedom adjustement (cf. [4]_ and [5]_).
+        Otherwise, the original Desparsified Lasso estimator is computed
+        (cf. [1]_ and [2]_ and [3]_).
+
+    confidence : float, optional (default=0.95)
         Confidence level used to compute the confidence intervals.
         Each value should be in the range [0, 1].
 
-    max_iter : int, optional
-        The maximum number of iterations (subproblem definitions).
+    max_iter : int, optional (default=5000)
+        The maximum number of iterations when regressing, by Lasso,
+        each column of the design matrix against the others.
 
-    tol : float, optional
-        The tolerance for the optimization: if the updates are
-        smaller than ``tol``, the optimization code checks the
-        dual gap for optimality and continues until it is smaller
-        than ``tol``.
+    tol : float, optional (default=1e-3)
+        The tolerance for the optimization of the Lasso problems: if the
+        updates are smaller than `tol`, the optimization code checks the
+        dual gap for optimality and continues until it is smaller than `tol`.
 
-    method : string, optional
-        The method for the nodewise lasso: "lasso" or "lasso_cv".
+    residual_method : string, optional (default='lasso')
+        The method for the computind the residuals of the Nodewise Lasso.
+        Currently the only method available is 'lasso'.
 
-    c : float, optional
-        Only used if method="lasso". Then alpha = c * alpha_max.
+    c : float, optional (default=0.01)
+        Only used if method='lasso'. Then alpha = c * alpha_max.
 
     n_jobs : int or None, optional (default=1)
-        Number of CPUs to use during the cross validation.
+        Number of CPUs to use during the Nodewise Lasso.
+
+    memory : str or joblib.Memory object, optional (default=None)
+        Used to cache the output of the computation of the Nodewise Lasso.
+        By default, no caching is done. If a string is given, it is the path
+        to the caching directory.
+
+    verbose: int, optional (default=1)
+        The verbosity level: if non zero, progress messages are printed
+        when computing the Nodewise Lasso in parralel.
+        The frequency of the messages increases with the verbosity level.
 
     Returns
     -------
@@ -97,8 +148,12 @@ def desparsified_lasso_confint(X, y, confidence=0.95, max_iter=5000,
 
     quantile = st.norm.ppf(1 - (1 - confidence) / 2)
 
-    if method == "lasso":
+    memory = check_memory(memory)
 
+    if normalize:
+
+        y = y - np.mean(y)
+        X = StandardScaler().fit_transform(X)
         Gram = np.dot(X.T, X)
 
         k = c * (1. / n_samples)
@@ -109,40 +164,35 @@ def desparsified_lasso_confint(X, y, confidence=0.95, max_iter=5000,
         Gram = None
         alphas = None
 
-    # Calculating Omega Matrix
-    results = \
-        Parallel(n_jobs=n_jobs)(
-            delayed(_compute_residuals)
-                (X=X,
-                 column_index=i,
-                 alpha=alphas[i],
-                 Gram=Gram,
-                 max_iter=max_iter,
-                 tol=tol,
-                 method=method,
-                 c=c)
-            for i in range(n_features))
-
-    results = np.asarray(results)
-    Z = np.stack(results[:, 0], axis=1)
-    omega_diag = np.stack(results[:, 1])
-
-    omega_invsqrt_diag = omega_diag ** (-0.5)
+    # Calculating precision matrix (Nodewise Lasso)
+    Z, omega_diag = memory.cache(_compute_all_residuals, ignore=['n_jobs'])(
+        X, alphas, Gram=Gram, max_iter=max_iter, tol=tol,
+        method=residual_method, c=c, n_jobs=n_jobs, verbose=verbose)
 
     # Lasso regression
-    clf_lasso_cv = LassoCV(cv=5, max_iter=max_iter, n_jobs=n_jobs)
-    clf_lasso_cv.fit(X, y)
-    beta_lasso = clf_lasso_cv.coef_
+    sigma_hat, beta_lasso = reid(X, y, n_jobs=n_jobs)
 
-    # Estimating the coefficient vector
-    beta_bias = y.T.dot(Z) / np.sum(X * Z, axis=0)
+    # Computing the degrees of freedom adjustement
+    if dof_ajdustement:
+        coef_max = np.max(np.abs(beta_lasso))
+        support = np.sum(np.abs(beta_lasso) > 0.01 * coef_max)
+        support = min(support, n_samples - 1)
+        dof_factor = n_samples / (n_samples - support)
+    else:
+        dof_factor = 1
+
+    # Computing Desparsified Lasso estimator and confidence intervals
+    beta_bias = dof_factor * np.dot(y.T, Z) / np.sum(X * Z, axis=0)
 
     P = ((Z.T.dot(X)).T / np.sum(X * Z, axis=0)).T
     P_nodiag = P - np.diag(np.diag(P))
+    Id = np.identity(n_features)
+    P_nodiag = dof_factor * P_nodiag + (dof_factor - 1) * Id
 
     beta_hat = beta_bias - P_nodiag.dot(beta_lasso)
 
-    sigma_hat, _ = reid(X, y)
+    omega_diag = omega_diag * dof_factor ** 2
+    omega_invsqrt_diag = omega_diag ** (-0.5)
 
     confint_radius = np.abs(quantile * sigma_hat /
                             (np.sqrt(n_samples) * omega_invsqrt_diag))
--- hidimstat/test/test_desparsified_lasso.py
+++ hidimstat/test/test_desparsified_lasso.py
@@ -2,31 +2,33 @@
 Test the desparsified_lasso module
 """
 
-import numpy as np
 from numpy.testing import assert_almost_equal
 
+from hidimstat.scenario import multivariate_1D_simulation
 from hidimstat.desparsified_lasso import desparsified_lasso_confint
 
 
 def test_desparsified_lasso_confint():
 
-    rng = np.random.default_rng(0)
+    n_samples, n_features = 20, 50
+    support_size = 1
+    sigma = 0.1
+    rho = 0.0
 
-    n_samples, n_features = 30, 30
-    n_support = 1
-    sigma = 0.02
+    X, y, beta, noise = \
+        multivariate_1D_simulation(n_samples=n_samples, n_features=n_features,
+                                   support_size=support_size, sigma=sigma,
+                                   rho=rho, shuffle=False, seed=5)
 
-    X = rng.normal(size=(n_samples, n_features))
-    beta = np.zeros(n_features)
-    beta[:n_support] = 1.0
-    epsilon = sigma * rng.normal(size=n_samples)
-    y = np.dot(X, beta) + epsilon
+    beta_hat, cb_min, cb_max = desparsified_lasso_confint(X, y)
 
-    beta_hat, cb_min, cb_max = desparsified_lasso_confint(X, y, n_jobs=1)
-    expected = np.array([beta, 0.03 * np.ones(n_features),
-                         0.06 * np.ones(n_features)])
+    assert_almost_equal(beta_hat, beta, decimal=1)
+    assert_almost_equal(cb_min, beta - 0.05, decimal=1)
+    assert_almost_equal(cb_max,  beta + 0.05, decimal=1)
 
-    for i in np.arange(expected.shape[1]):
-        assert_almost_equal(beta_hat[i], expected[0, i], decimal=1)
-        assert_almost_equal(beta_hat[i] - cb_min[i], expected[1, i], decimal=1)
-        assert_almost_equal(cb_max[i] - cb_min[i], expected[2, i], decimal=1)
+    beta_hat, cb_min, cb_max = \
+        desparsified_lasso_confint(X, y, normalize=True, dof_ajdustement=True)
+
+    assert_almost_equal(beta_hat, beta, decimal=1)
+    assert_almost_equal(cb_min, beta - 0.05, decimal=1)
+    assert_almost_equal(cb_max,  beta + 0.05, decimal=1)
