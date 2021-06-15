429ae3f70356479820ecb6e6f02dbb4b8b7176d4
jerome-alexis_chevalier <jerome-alexis.chevalier@inria.fr>
2021-06-15 19:09:58 +0200

    Modifying a bit ensemble_clustered_inference to remove errors (more to do here)

--- hidimstat/ensemble_clustered_inference.py
+++ hidimstat/ensemble_clustered_inference.py
@@ -5,7 +5,8 @@ from .multi_sample_split import aggregate_medians, aggregate_quantiles
 from .clustered_inference import clustered_inference
 
 
-def ensemble_clustered_inference(X_init, y, ward, n_clusters, method='DL',
+def ensemble_clustered_inference(X_init, y, ward, n_clusters,
+                                 method='desparsified-lasso',
                                  aggregate='quantiles', gamma_min=0.2,
                                  train_size=0.7, condition_mask=None,
                                  groups=None, seed=0, n_rand=25, predict=False,
@@ -13,17 +14,17 @@ def ensemble_clustered_inference(X_init, y, ward, n_clusters, method='DL',
     """EnCluDL"""
 
     results = Parallel(n_jobs=n_jobs)(
-        delayed(clustered_inference)(X_init, y, ward, n_clusters, method,
-                                     train_size, condition_mask, groups, rand,
-                                     predict)
+        delayed(clustered_inference)(X_init, y, ward, n_clusters, train_size,
+                                     groups, method, rand)
         for rand in np.arange(seed, seed + n_rand))
 
     results = np.asarray(results)
 
-    list_sf = results[:, 0, :]
-    list_sf_corr = results[:, 1, :]
-    list_cdf = results[:, 2, :]
-    list_cdf_corr = results[:, 3, :]
+    # list_beta_hat = results[:, 0, :]
+    list_sf = results[:, 1, :]
+    list_sf_corr = results[:, 2, :]
+    list_cdf = results[:, 3, :]
+    list_cdf_corr = results[:, 4, :]
 
     if aggregate == 'quantiles':
 
@@ -38,12 +39,5 @@ def ensemble_clustered_inference(X_init, y, ward, n_clusters, method='DL',
         sf_corr = aggregate_medians(list_sf_corr)
         cdf = aggregate_medians(list_cdf)
         cdf_corr = aggregate_medians(list_cdf_corr)
-
-    if predict:
-
-        list_beta_hat = results[:, 4, :]
-        beta_hat = np.mean(np.asarray(list_beta_hat), axis=0)
-
-        return sf, sf_corr, cdf, cdf_corr, beta_hat
 
     return sf, sf_corr, cdf, cdf_corr
