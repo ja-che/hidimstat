2ea6a6a28240e60bd36077bc3a0c3d23a352bf6a
Guillaume Favelier <guillaume.favelier@gmail.com>
2021-06-25 13:42:49 +0200

    Add brain_scraper

--- doc/conf.py
+++ doc/conf.py
@@ -221,7 +221,8 @@ try:
 except Exception:
     pass
 else:
-    scrapers += ('pyvista',)
+    brain_scraper = mne.viz._brain._BrainScraper()
+    scrapers += (brain_scraper, 'pyvista')
 if any(x in scrapers for x in ('pyvista')):
     from traits.api import push_exception_handler
     push_exception_handler(reraise_exceptions=True)
